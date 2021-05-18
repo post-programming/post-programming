982b8ed59c1b25aeedcd26e1c6e78db99391bb3b
Prateek Rastogi <prateek.rastogi@pm.me>
2021-05-19 00:13:33 +0530

    hyperTik.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -63,7 +63,7 @@ module.exports = {
           items: [
             {
               label: 'Twitter',
-              href: 'https://twitter.com/swapZapp'
+              href: 'https://twitter.com/hyperTik'
             }
           ]
         }
